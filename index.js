06bba3dc37b4c75d111b1e32422bdb58e1d84d0b
Miroslav Popov <info@forexsb.com>
2022-08-31 19:53:10 +0000

    It accepts argv.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict'
-
-const process  = require('node:process');
-
 const {toCamelCase}    = require('@popovmp/camel-case')
 const {parseJsonValue} = require('@popovmp/json-value-parser')
 
 /**
  * Parses the CLI options
  *
- * @param {string[]} [argv]
+ * @param {string[]} argv
  *
  * @return {Object}
  */
@@ -16,8 +13,6 @@ function parse(argv)
 {
 	const options = {}
 	let lastKey = 'subcommand'
-
-	const args = argv || process.argv
 
 	const pushValue = (key, value) => {
 		const val = parseJsonValue(value)
@@ -33,7 +28,7 @@ function parse(argv)
 		}
 	}
 
-	for (const arg of args) {
+	for (const arg of argv) {
 		const input = arg.trim()
 
 		if (input === '=') continue
